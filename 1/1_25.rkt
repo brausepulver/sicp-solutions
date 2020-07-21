5621f32de4cf0ae7c41f7dae2b703d1b899abab2
brausepulver <34420739+brausepulver@users.noreply.github.com>
2020-07-22 01:20:10 +0200

    Fix Proof in 1.25 (Warning: Advanced Maffs)

--- 1/1_25.rkt
+++ 1/1_25.rkt
@@ -20,10 +20,14 @@
 (expmod-alt 13 7 7)
 ; Both 6.
 
-; Given that, for an exponent e that is even, expmod computes (b^(e/2) mod m)^2 mod m, and expmod-alt
-; computes (b^(e/2))^2 mod m, it follows that (b^(e/2) mod m)^2 cong. (b^(e/2))^2 (mod m).
-; By the properties of modular arithmetic, (b^(e/2) mod m) cong. (b^(e/2)) (mod m).
-; But since (b^(e/2) mod m) mod m = b^(e/2) mod m, the two procedures compute the same result.
+; For an exponent e that is even, expmod computes (b^(e/2) mod m)^2 mod m. Let c be an integer such
+; that (b^(e/2) mod m)^2 cong. c^2 (mod m). Then, by the properties of modular arithmetic,
+; (b^(e/2) mod m) cong. c (mod m). But since (b^(e/2) mod m) mod m = b^(e/2) mod m, b^(e/2) cong. c (mod m).
+
+; On the other hand, expmod-alt computes (b^(e/2))^2 mod m. Let d be an integer such that
+; (b^(e/2))^2 cong. d^2 (mod m). Then, b^(e/2) cong. d (mod m), so that c cong. d (mod m). But then,
+; c^2 cong. d^2 (mod m) so that (b^(e/2) mod m)^2 mod m = (b^(e/2))^2 mod m. Therefore the procedures
+; compute the same result.
 
 ; Although they compute the same result, they do not serve equally well. While in expmod the numbers
 ; used throughout the computation do not get much larger than m, in expmod-alt b^e is directly compu-
