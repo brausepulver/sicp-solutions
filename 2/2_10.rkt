c2c28a954b69ef9ae577cc418aa4470d4cfd69d1
brausepulver <34420739+brausepulver@users.noreply.github.com>
2020-08-19 22:20:16 +0200

    Fix Exercise 2.10 Previously I was assuming the phrase 'spans zero' from the book to mean an interval has a width of 0, when actually it means an interval 'con- tains' zero so to speak. I also fixed the order of the arguments to the make-interval procedure when mul-interval is called.

--- 2/2_10.rkt
+++ 2/2_10.rkt
@@ -7,7 +7,14 @@
 ; Problem: Modify div-interval to check for zero division and to signal an error if it occurs.
 
 (define (div-interval x y)
-    (if (= (upper-bound y) (lower-bound y))
+    (define (spans-zero? i) (and (>= (upper-bound i) 0) (<= (lower-bound i) 0)))
+    (if (spans-zero? y)
         (error "Division by interval that spans zero.")
-        (mul-interval x (make-interval (/ 1.0 (lower-bound y))
-                                       (/ 1.0 (upper-bound y))))))
+        (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+                                       (/ 1.0 (lower-bound y))))))
+
+; (div-interval (make-interval 1 2) (make-interval 4 10))
+; (0.5 . 0.1)
+
+; (div-interval (make-interval 1 2) (make-interval -4 10))
+; Division by interval that spans zero.
